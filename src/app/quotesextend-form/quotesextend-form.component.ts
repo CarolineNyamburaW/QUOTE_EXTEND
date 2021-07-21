7405efab3a775d17e16cf0a20673d3263d213ad4
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-07-21 12:41:18 +0300

    modified  quotesextendform.component.ts

--- src/app/quotesextend-form/quotesextend-form.component.ts
+++ src/app/quotesextend-form/quotesextend-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-quotesextend-form',
@@ -6,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotesextend-form.component.css']
 })
 export class QuotesextendFormComponent implements OnInit {
+
+  @Output() charles= new EventEmitter();
+
+  submit(me:any, ga:NgForm){
+    if(me.yourName=="" || me.date=="" || me.author=="" || me.quote==""){
+      alert("Kindly fill the form")
+    }else{
+      this.charles.emit(me);
+      ga.reset()
+    }
+  };
 
   constructor() { }
 
