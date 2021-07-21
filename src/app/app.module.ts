f34a7916bdc892d8a46b4b736c49541e363e314c
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-07-21 03:37:18 +0300

    created a div class for the trending quotes section in quotesdetailscomponent.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesdetailComponent } from './quotesdetail/quotesdetail.component';
@@ -17,6 +18,7 @@ import { VotesComponent } from './votes/votes.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
+ 
+  constructor(public yourName:string, public quote:string, public author:string){
+    
+  }
+
 }
