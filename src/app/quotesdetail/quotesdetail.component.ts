30544790bcebdf05f2ddeb9076902c709a21bd9a
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-07-21 04:27:57 +0300

    modified quotedetail.component.ts

--- src/app/quotesdetail/quotesdetail.component.ts
+++ src/app/quotesdetail/quotesdetail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotesdetail',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotesdetail.component.css']
 })
 export class QuotesdetailComponent implements OnInit {
+
+    
+  carol:Quote[]=[
+    new Quote("Caroh", "Life is a succession of lessons which must be lived to be understood.", "- Ralph Waldo Emerson"),
+    new Quote("Victor", "You can always edit a bad page. You can’t edit a blank page", "- Jodi Picoult"),
+    new Quote("Faith", "I'm writing a first draft and reminding myself that I'm simply shoveling sand into a box so that later I can build castles.", "- Shannon Hale"),
+
+  ]
+
+  carbon(event:any){
+    this.carol.push(event);
+    
+  }
+  
+
 
   constructor() { }
 
