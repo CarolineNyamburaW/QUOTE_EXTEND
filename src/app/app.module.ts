d2e1bc3aeb578fcdc2e079d0fbb67ed0e2c0bdc0
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-07-21 03:45:14 +0300

    styles div the div class for trending quotes and its components in quotesdetailscomponent.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesdetailComponent } from './quotesdetail/quotesdetail.component';
@@ -18,7 +18,7 @@ import { VotesComponent } from './votes/votes.component';
   imports: [
     BrowserModule,
     AppRoutingModule
-    FormsModule
+    // FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
